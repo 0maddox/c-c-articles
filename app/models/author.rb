616d6cfc15e578a3912095678d130b2ad43a7868
nicholas <nickkiim7@gmail.com>
2022-10-31 15:04:43 +0300

    wip

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,28 @@
+require_relative './article'
+
 class Author
-  attr_accessor :name
-
-
+  attr_reader :name
+  @@all = []
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def articles
+    Article.all.filter {|article| article.author == self}
+  end
 
-end
+  def magazines
+    self.articles.map {|article| article.magazine}.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+      self.magazines.map {|magazine|magazine.category}.uniq
+  end
+  def self.all
+    @@all
+  end
